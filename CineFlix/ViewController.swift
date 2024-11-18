588d4157c275145c5998d89cb4ad6d755734d9f6
Rajat Sachdeva <r.sachdeva3105@gmail.com>
2024-11-17 22:06:03 -0500

    Initial Commit

---
+++ CineFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineFlix
+//
+//  Created by Rajat Sachdeva on 2024-11-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
